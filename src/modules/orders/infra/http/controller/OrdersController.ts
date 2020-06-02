18d5016495eb3ef97b8424c9320a658bcf902780
Cassio C <LccassioL@gmail.com>
2020-06-02 17:04:10 -0300

    Remove todo tag

--- src/modules/orders/infra/http/controller/OrdersController.ts
+++ src/modules/orders/infra/http/controller/OrdersController.ts
@@ -16,7 +16,6 @@ export default class OrdersController {
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
     const { customer_id, products } = request.body;
     const createOrderService = container.resolve(CreateOrderService);
     const order = await createOrderService.execute({ customer_id, products });
